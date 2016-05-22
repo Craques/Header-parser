816ee635dfe841d6775e1f36eb7d89a2604651d7
craques <charlesmuz2006@gmail.com>
2016-05-22 12:41:51 -0700

    added favicon.ico

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var express = require("express");
 var app = express();
 var handlebars = require("express-handlebars");
 var routes = require("./routes/index.js");
+var favicon = require("serve-favicon");
 
 //enable stylesheet and js files to use public route
 app.use('/public', express.static(process.cwd() + "/public"));
+app.use(favicon(__dirname + "/favicon.ico"));
 
 //express-handlebars will be my view engine
 app.set("views", __dirname +"/views");
